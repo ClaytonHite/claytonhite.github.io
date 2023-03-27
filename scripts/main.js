cdff504515c6df429bc1ac8406f9899ac946858a
Clayton Hite <clay054@aol.com>
2023-03-27 09:20:23 -0400

    naming changes correction

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const mobileMenuButton = document.getElementById('mobileNavMenu');
+const mobileMenuButton = document.getElementById('mobileNavMenuButton');
 const mobileMenu = document.querySelector('.nav-links-container');
 
 mobileMenuButton.addEventListener('click', () => {
